859877ebf95a100447da536cfbdd2a4a2d176360
Daniil <85370294+danillnovikov@users.noreply.github.com>
2022-02-21 00:26:35 +0300

    changed button name

---
+++ Login.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Login.css';
+import { Form, Input, Button } from 'antd';
+
+const Login = () => {
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 4,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+    >
+      <Form.Item label="Username" name="username">
+        <Input placeholder="Email" />
+      </Form.Item>
+
+      <Form.Item label="Password" name="password">
+        <Input.Password placeholder="Password" />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 11,
+          span: 16,
+        }}
+      >
+        <Button>Submit</Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Login;
